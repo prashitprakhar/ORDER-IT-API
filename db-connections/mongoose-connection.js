6ff80b3130610ce6b877d4f038ae3da5b2897e01
Prashit Prakhar <prashitprakhar@yahoo.com>
2020-03-18 02:19:47 +0530

    updates in mongoose connection

--- db-connections/mongoose-connection.js
+++ db-connections/mongoose-connection.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/ORDER_IT_DB', { useFindAndModify: false, autoIndex: false });
+// mongoose.connect('mongodb://localhost:27017/ORDER_IT_DB', { useFindAndModify: false, autoIndex: false });
 // mongoose.connect('mongodb://indilligence123:indilligence123@ds243317.mlab.com:43317/indilligencedb');
-// mongoose.connect('mongodb://orderitservices:orderitservices123@ds043942.mlab.com:43942/order_it_db', { useFindAndModify: false });
+mongoose.connect('mongodb://orderitservices:orderitservices123@ds043942.mlab.com:43942/order_it_db', { useFindAndModify: false });
 
 module.exports = { mongoose };
 
